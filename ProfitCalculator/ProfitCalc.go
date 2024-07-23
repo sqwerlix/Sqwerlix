75f8d47e6025c420c62357a915c537b37a069dd9
Philip <phiber90@gmail.com>
2024-07-23 08:52:20 +0200

    feat: Refactor profit calculation and input handling
    ### Features
    - Introduced `readUserInput` function to handle user input for revenue, expenses, and tax rate.
    - Added `calculateProfit` function to encapsulate the profit calculation logic.
    
    ### Changes
    - Replaced direct `fmt.Scan` calls with `readUserInput` function to improve code readability and reusability.
    - Modified `main` function to use the new `readUserInput` and `calculateProfit` functions.
    - Updated the output format in the `main` function to display ETB, Profit, Ratio, and Percentage Profit.
    
    ### Removed
    - Removed inline `fmt.Scan` calls for revenue, expenses, and tax rate.

--- ProfitCalculator/ProfitCalc.go
+++ ProfitCalculator/ProfitCalc.go
@@ -6,20 +6,31 @@ import (
 )
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
 
-	fmt.Println("Enter revenue")
-	fmt.Scan(&revenue)
-	fmt.Println("Enter expenses")
-	fmt.Scan(&expenses)
-	fmt.Println("Enter tax rate")
-	fmt.Scan(&taxRate)
+	//fmt.Println("Enter revenue")
+	revenue := readUserInput("Enter revenue: ")
+	//fmt.Println("Enter expenses")
+	expenses := readUserInput("Enter expenses: ")
+	//fmt.Println("Enter tax rate")
+	taxRate := readUserInput("Enter tax rate: ")
 
+	ebt, profit, ratio, percentage_profit := calculateProfit(revenue, expenses, taxRate)
+	fmt.Printf("ETB: %v\nProfit: %v\nRatio: %.2f%%\nPercentage profit: %.2f%%", ebt, profit, ratio, percentage_profit)
+}
+
+// Read user input function
+func readUserInput(InfoText string) float64 {
+	var userInput float64
+	fmt.Print(InfoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+// Calculate profit function
+func calculateProfit(revenue, expenses, taxRate float64) (float64, float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := (profit / ebt) * 100
 	percentage_profit := (profit / revenue) * 100
-	fmt.Printf("ETB: %v\nRatio: %.2f%%\nProfit: %v\nPercentage Profit: %.2f%%", ebt, ratio, profit, percentage_profit)
+	return ebt, profit, ratio, percentage_profit
 }
